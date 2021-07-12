b223f98a8a13d8f1c58b713eca5af6bccd6697d1
Stephen Combs <combss3@udayton.edu>
2021-07-12 18:57:37 -0400

    - Added Staff headshots & bios - Added footer with copyrighting - Added new logo - Added information to Story Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarModule } from './components/navbar/navbar.module';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, FooterComponent],
   imports: [BrowserModule, AppRoutingModule, NavbarModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/pages/staff/staff.component.ts
+++ src/app/pages/staff/staff.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-staff',
@@ -28,31 +28,29 @@ export class StaffComponent implements OnInit {
     {
       img: '../../../assets/default-avatar.jpg',
       name: 'Stanley Scheidler, D.O.',
+      description: `Dr. Stan is a patient-focused primary care physician dedicated to serving his community's families.  He believes that everyone needs to be treated as a person, not as a number or a group.  He understands that a patient is a person who is a unique individual and who should be addressed in that manner.`,
+    },
+    {
+      img: '../../../assets/headshots/roberson.svg',
+      name: 'Chris Roberson, NP',
       description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lorem orci, sollicitudin sit amet ex at, maximus ornare eros. Donec et mauris hendrerit, sollicitudin urna sit amet, sollicitudin elit. Maecenas ante magna, vehicula quis dolor vitae, pharetra varius quam. Sed et tincidunt nibh. Phasellus vitae egestas felis. Cras luctus a.`,
     },
     {
-      img: '../../../assets/default-avatar.jpg',
-      name: 'Joseph Scheidler, D.O.',
-      description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lorem orci, sollicitudin sit amet ex at, maximus ornare eros. Donec et mauris hendrerit, sollicitudin urna sit amet, sollicitudin elit. Maecenas ante magna, vehicula quis dolor vitae, pharetra varius quam. Sed et tincidunt nibh. Phasellus vitae egestas felis. Cras luctus a.`,
-    },
-    {
-      img: '../../../assets/default-avatar.jpg',
-      name: `Brianna O'Neill, Scribe`,
-      description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lorem orci, sollicitudin sit amet ex at, maximus ornare eros. Donec et mauris hendrerit, sollicitudin urna sit amet, sollicitudin elit. Maecenas ante magna, vehicula quis dolor vitae, pharetra varius quam. Sed et tincidunt nibh. Phasellus vitae egestas felis. Cras luctus a.`,
-    },
-    {
-      img: '../../../assets/default-avatar.jpg',
-      name: 'Stanley Scheidler, D.O.',
-      description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lorem orci, sollicitudin sit amet ex at, maximus ornare eros. Donec et mauris hendrerit, sollicitudin urna sit amet, sollicitudin elit. Maecenas ante magna, vehicula quis dolor vitae, pharetra varius quam. Sed et tincidunt nibh. Phasellus vitae egestas felis. Cras luctus a.`,
-    },
-    {
-      img: '../../../assets/default-avatar.jpg',
-      name: 'Stanley Scheidler, D.O.',
-      description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lorem orci, sollicitudin sit amet ex at, maximus ornare eros. Donec et mauris hendrerit, sollicitudin urna sit amet, sollicitudin elit. Maecenas ante magna, vehicula quis dolor vitae, pharetra varius quam. Sed et tincidunt nibh. Phasellus vitae egestas felis. Cras luctus a.`,
+      img: '../../../assets/headshots/gaitskill.svg',
+      name: `Christian Gaitskill, MSW, LSW`,
+      description: `Christian has been a Licensed Social Worker for 4 years after having obtained her Master’s in Social Work from Northern Kentucky University. Christian’s primary focus is domestic violence and child abuse, but loves working with individuals experiencing anxiety, depression, and post-traumatic stress disorder among other common disorders. To get scheduled with Christian, please call 513-286-3043. `,
     },
   ];
+
+  numVisible = 3;
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: { target: { innerWidth: any } }) {
+    if (event.target.innerWidth <= 768) this.numVisible = 1;
+    else this.numVisible = 3;
+  }
 }
