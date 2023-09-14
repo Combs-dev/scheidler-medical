52526d879c485034aea35fc5c1a9bc492d36100f
Stephen Combs <scombs@paycor.com>
2023-09-14 12:55:05 -0400

    Added Forms page & Netlify adapter

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,21 +1,20 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
-import react from '@astrojs/react'
-import sitemap from '@astrojs/sitemap'
-import sanity from 'astro-sanity'
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
+import sanity from 'astro-sanity';
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://scheidlermedical.com',
-    integrations: [
-        tailwind(),
-        react(),
-        sitemap(),
-        sanity({
-            projectId: 'vfh8d0hi',
-            dataset: 'production',
-            apiVersion: '2021-03-25',
-            useCdn: true,
-        }),
-    ],
-})
+  site: 'https://scheidlermedical.com',
+  integrations: [tailwind(), react(), sitemap(), sanity({
+    projectId: 'vfh8d0hi',
+    dataset: 'production',
+    apiVersion: '2021-03-25',
+    useCdn: true
+  })],
+  output: "server",
+  adapter: netlify()
+});
