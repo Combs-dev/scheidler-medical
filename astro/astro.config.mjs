320a58f86ebb2e3d5844885022d62afc2fbcca76
Stephen Combs <scombs@paycor.com>
2023-09-14 14:35:11 -0400

    Add Astro webmanifest integration

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -3,8 +3,9 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import sanity from 'astro-sanity';
-
 import netlify from "@astrojs/netlify/functions";
+
+import webmanifest from "astro-webmanifest";
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,7 +15,7 @@ export default defineConfig({
     dataset: 'production',
     apiVersion: '2021-03-25',
     useCdn: true
-  })],
+    }), webmanifest({name: "Scheidler Medical Preferred"})],
   output: "server",
   adapter: netlify()
 });
